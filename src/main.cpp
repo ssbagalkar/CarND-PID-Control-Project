c0412e53f11c4f4a82f7e820e0a1841dfe84654e
Saurabh Bagalkar <sbagalka@asu.edu>
2017-09-24 17:58:09 -0500

    modify initial settings

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <uWS/uWS.h>
+#define _USE_MATH_DEFINES // for C++
+#include <cmath>
 #include <iostream>
 #include "json.hpp"
 #include "PID.h"
 #include <math.h>
+#define _USE_MATH_DEFINES // for C++
+#include <cmath>
 
 // for convenience
 using json = nlohmann::json;
